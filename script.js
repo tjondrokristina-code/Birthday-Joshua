a6fb7a38b83b33b3340246bcbed7b639a6199e69
tjondrokristina-code <tjondrokristina@gmail.com>
2026-06-30 23:05:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById("startBtn").addEventListener("click",function(){
-alert("Happy Birthday Beb Joshua ❤️\n\nPerjalananmu baru dimulai...");
+const btn = document.getElementById("startBtn");
+
+btn.addEventListener("click",function(){
+
+document.body.style.transition="1s";
+document.body.style.background="#000";
+
+setTimeout(()=>{
+alert("🎉 Selamat datang Beb Joshua ❤️\n\nPetualangan ulang tahunmu dimulai...");
+},600);
+
 });
